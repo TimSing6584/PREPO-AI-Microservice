72cf86543c55ce43ee112783ce2e2ff5ddf5a1dc
Tuan Pham <phamtranminhtuan2006@gmail.com>
2026-06-09 23:47:18 +0700

    add router import to main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .exceptions import AppException, app_exception_handler
 from .speech_to_text.router import router as stt_router
+from .assessment.router import router as assessment_router
+from .pipeline.router import router as pipeline_router
 
 app = FastAPI(title="Speech Assessment Microservice")
 
